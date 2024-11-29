d06f2554db3ed7ee6bed2465941f936982bda86e
SafalPaudel <safalp2061@gmail.com>
2024-11-29 21:22:00 +0545

    Late Study

--- Day 19-20.py
+++ Day 19-20.py
@@ -1,2 +1,54 @@
-x = int(input("Enter the number:"))
-print("The number is :", x)
+# for i in range(11):
+#     if (i==10):
+#         print("Leave the loop")
+#         continue
+#     print("5 X", i , "=", 5*i)
+
+
+
+# for i in range(1,101,1):
+#     print(i )
+#     if(i==50):
+#         break
+#     else:
+#         print("Mississippi")
+# print("Thank you")
+
+
+
+# for i in [2,3,4,6,8,0]:
+#     if (i%2!=0):
+#         continue
+#     print(i)
+
+
+################## FUNCTION #################333
+
+def calculateGmean( a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isGreater(a,b):
+    if(a>b):
+        print("First number is greater")
+    else:
+        print("Second number is greater")
+
+a=9
+b=8
+isGreater(a,b)
+# gmean1 = (a*b)/(a+b)
+# print(gmean1)
+calculateGmean(a,b)
+
+def isSmaller(c,d):
+    pass
+
+c=8
+d=7
+#gmean2 = (c*d)/(c+d)
+#print(gmean2)
+calculateGmean(c,d)
+
+
+
