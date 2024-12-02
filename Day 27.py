4dfd44d523fdbb69312b5ee9556359a4fe89562c
SafalPaudel <safalp2061@gmail.com>
2024-12-02 21:54:39 +0545

    KBC Game

---
+++ Day 27.py
@@ -0,0 +1,31 @@
+
+print("Welcome to KO BANCHA CROREPATI")
+
+a = input("What if the capital city of Nepal?")
+
+
+if(a=="kathmandu"):
+    print("Its a right answer . You won 1000Rs")
+    b = input("What is the full form of WWW?")
+
+    if(b=="world wide web"):
+        print("Its a right answer . You won 5000Rs")
+        c=input("Which company manufactured Brezza Car?")
+
+        if(c=="maruti suzuki"):
+            print("Its a right answer .You won 10000Rs")
+        else:
+            print("Its a wrong answer. Thanks for playing")
+    else:
+        print("Its a wrong answer . Thanks for playing")
+else:
+    print("Its a wrong answer . Thanks for playing")
+
+
+
+
+
+
+
+
+
