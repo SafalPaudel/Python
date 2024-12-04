f7b429c6aaafb866c6b1709c883c4fae59850400
SafalPaudel <safalp2061@gmail.com>
2024-12-04 21:43:38 +0545

    F-Strings

---
+++ Day 28.py
@@ -0,0 +1,13 @@
+letter = "My name is {} and I am from {}"
+name="Safal"
+country="Nepal"
+print(letter.format(name,country))
+
+print(f"My name is {name} and I am from {country}")
+
+
+txt = "For only {price:.2f} dollars!"
+print(txt.format(price = 49))
+
+print(f"3*10")
+print(type(f"3*10"))
